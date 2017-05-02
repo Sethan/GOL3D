453fc56d33615b6a0fd59479e4adcf3b1680246e
TenorSax <kavkaz2005_@hotmail.com>
2017-05-02 12:46:26 +0200

    Update CellGraphTop.java

--- model/CellGraphTop.java
+++ model/CellGraphTop.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
  *
  * @author ZuraH
  */
+ // Koden utføre det samme som når CellGrapfLeft dannes, men CellGrapfTop har annet dimisjon og dimisjonen er (19,1,19)
 public class CellGraphTop extends CellGraph{
     public CellGraphTop(int x, int y)
     {
@@ -25,6 +26,14 @@ public class CellGraphTop extends CellGraph{
         }
     }
     
+     //teller naboene til en bestemt cell som lever eller ikke lever. 
+    // Dette utføres ved at den vil starte med ei celle som for eksempel har posisjonen 12,9,. Koden vil da ta koordinatene den fikk og 
+    //starte med 11-1 og 14-1.(i vil gjelde for 11 og n vil gjelde for 14)
+    // også vil den jobbe seg opp til 11+1, 14+1. Den vil unngå n=0 og i= 0 fordi dette er den opprinlige cellen, siden programmeret skal 
+    //representere 3d modell så har vi en bestemt tilfelle når vi kommer utenfor lengeden til tabellen.
+    // Hvis y + n er større enn høyden - 1 og hvis venstre grafen's (x + i, siste element i høyden) hvis denne lever så telles den som nabo, 
+    //og hvis x + i er større enn bredden
+    // så bruker vi høyre graf sin (y+n, siste element i bredden) sjekker vi også om denne lever og om den lever teller vi den som nabo.
     @Override
     public int countNeighbours(int x, int y, CellGraph3D cg3d)
     {
